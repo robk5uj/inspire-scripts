787439b85d7413410330b87bd765ddb4b44479cd
Alessio Deiana <alessio.deiana@cern.ch>
2013-03-19 17:54:55 +0100

    Removes BibRecord
    BibRecord has been moved into invenio, please import it like this
    from invenio.docextract_record import ...

--- datacleaning/job_helper.py
+++ datacleaning/job_helper.py
@@ -2,14 +2,11 @@ import os
 import time
 
 from tempfile import mkstemp
-from operator import itemgetter
-# from xml.etree import ElementTree as ET
-import ElementTree as ET
 
 from invenio.dbquery import run_sql
 from invenio.config import CFG_TMPSHAREDDIR
 from invenio.bibtask import task_low_level_submission
-from invenio.search_engine import get_record as get_record_original
+
 
 def submit_bibupload_task(to_submit, mode, user, priority=3, notimechange=False):
     # Save new record to file
@@ -92,120 +89,3 @@ def loop(recids, callback):
         callback(recid)
         if done % 50 == 0:
             print 'done %s of %s' % (done + 1, len(recids))
-
-
-def get_record(recid):
-    def create_control_field(inst):
-        return BibRecordControlField(inst[3].decode('utf-8'))
-
-    def create_field(inst):
-        subfields = [BibRecordSubField(code, value.decode('utf-8')) \
-                                                for code, value in inst[0]]
-        return BibRecordField(ind1=inst[1], ind2=inst[2], subfields=subfields)
-
-    record = BibRecord()
-    for tag, instances in get_record_original(recid).iteritems():
-        if tag.startswith('00'):
-            record[tag] = [create_control_field(inst) for inst in instances]
-        else:
-            record[tag] = [create_field(inst) for inst in instances]
-
-    return record
-
-
-class BibRecord(object):
-    def __init__(self, recid=None):
-        self.record = {}
-        if recid:
-            self.record['001'] = [BibRecordControlField(str(recid))]
-
-    def __setitem__(self, tag, fields):
-        self.record[tag] = fields
-
-    def __getitem__(self, tag):
-        return self.record[tag]
-
-    def __eq__(self, b):
-        if set(self.record.keys()) != set(b.record.keys()):
-            return False
-
-        for tag, fields in self.record.iteritems():
-            if set(fields) != set(b[tag]):
-                return False
-
-        return True
-
-    def __hash__(self):
-        return hash(tuple(self.record.iteritems()))
-
-    def __repr__(self):
-        if '001' in self.record:
-            s = u'BibRecord(%s)' % list(self['001'])[0].value
-        else:
-            s = u'BibRecord()'
-        return s
-
-    def to_xml(self):
-        root = ET.Element('record')
-        for tag, fields in sorted(self.record.iteritems(), key=itemgetter(0)):
-            for field in fields:
-                if tag.startswith('00'):
-                    controlfield = ET.SubElement(root,
-                                                 'controlfield',
-                                                 {'tag': tag})
-                    controlfield.text = field.value
-                else:
-                    attribs = {'tag': tag,
-                               'ind1': field.ind1,
-                               'ind2': field.ind2}
-                    datafield = ET.SubElement(root, 'datafield', attribs)
-                    for subfield in field.subfields:
-                        attrs = {'code': subfield.code}
-                        s = ET.SubElement(datafield, 'subfield', attrs)
-                        s.text = subfield.value
-        return ET.tostring(root)
-
-
-class BibRecordControlField(object):
-    def __init__(self, value):
-        self.value = value
-
-    def __eq__(self, b):
-        return self.value == b.value
-
-    def __hash__(self):
-        return hash(self.value)
-
-
-class BibRecordField(object):
-    def __init__(self, ind1=" ", ind2=" ", subfields=None):
-        self.ind1 = ind1
-        self.ind2 = ind2
-        if subfields is None:
-            subfields = []
-        self.subfields = subfields
-
-    def __repr__(self):
-        return 'BibRecordField(ind1="%s",ind2="%s", subfields=%s)' % (self.ind1, self.ind2, self.subfields)
-
-    def __eq__(self, b):
-        return self.ind1 == b.ind1 and self.ind2 == b.ind2 \
-                    and set(self.subfields) == set(b.subfields)
-
-    def __hash__(self):
-        return hash((self.ind1, self.ind2, tuple(self.subfields)))
-
-    def get_subfield_values(self, code):
-        return [s.value for s in self.subfields if s.code == code]
-
-
-class BibRecordSubField(object):
-    def __init__(self, code, value):
-        self.code = code
-        self.value = value
-
-    def __eq__(self, b):
-        return self.code == b.code and self.value == b.value
-
-    def __hash__(self):
-        return hash((self.code, self.value))
--- datacleaning/job_helper_tests.py
+++ datacleaning/job_helper_tests.py
@@ -4,14 +4,8 @@ from doctest import Example
 
 from lxml.doctestcompare import LXMLOutputChecker
 
-from datacleaning.job_helper import get_record, \
-                                    all_recids, \
-                                    ChunkedTask, \
-                                    BibRecord
-
-from invenio.search_engine import get_record as get_record_original
-from invenio.search_engine import perform_request_search
-from invenio.bibrecord import print_rec
+from datacleaning.job_helper import all_recids, \
+                                    ChunkedTask
 
 
 class XmlTest(unittest.TestCase):
@@ -20,58 +14,6 @@ class XmlTest(unittest.TestCase):
         if not checker.check_output(want, got, 0):
             message = checker.output_difference(Example("", want), got, 0)
             raise AssertionError(message)
-
-
-class BibRecordTest(XmlTest):
-    def setUp(self):
-        from invenio import bibrecord
-
-        def order_by_tag(field1, field2):
-            """Function used to order the fields according to their tag"""
-            return cmp(field1[0], field2[0])
-        bibrecord._order_by_ord = order_by_tag
-
-        self.records_cache = {}
-        self.xml_cache = {}
-        for recid in perform_request_search(p=""):
-            record = get_record(recid)
-            self.records_cache[recid] = record
-            self.xml_cache[recid] = record.to_xml()
-
-    def test_get_record(self):
-        for recid in perform_request_search(p=""):
-            # Our bibrecord we want to test
-            record = self.records_cache[recid]
-            # Reference implementation
-            original_record = get_record_original(recid)
-
-            self.assertXmlEqual(record.to_xml(), print_rec(original_record))
-
-    def test_equality(self):
-        for recid in self.records_cache.iterkeys():
-            for recid2 in self.records_cache.iterkeys():
-                record = self.records_cache[recid]
-                xml = self.xml_cache[recid]
-                if recid == recid2:
-                    record2 = get_record(recid)
-                    xml2 = record2.to_xml()
-                    self.assertEqual(record, record2)
-                    self.assertXmlEqual(xml, xml2)
-                else:
-                    record2 = self.records_cache[recid2]
-                    xml2 = self.xml_cache[recid2]
-                    self.assertNotEqual(record, record2)
-
-    def test_hash(self):
-        for recid, original_record in self.records_cache.iteritems():
-            # Our bibrecord we want to test
-            record = BibRecord()
-
-            for tag, fields in original_record.record.iteritems():
-                record[tag] = list(set(fields))
-                self.assertEqual(set(record[tag]), set(original_record[tag]))
-
-            self.assertEqual(record, original_record)
 
 
 class MiscTest(unittest.TestCase):
