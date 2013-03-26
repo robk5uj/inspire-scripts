91355a68a4ac3136e03b5d98d41a74102bc6c07d
Alessio Deiana <alessio.deiana@cern.ch>
2013-03-26 14:52:07 +0100

    Updates refextract tester

--- test_refextract.py
+++ test_refextract.py
@@ -1,16 +1,18 @@
 import os
+import sys
 
-from ids import SELECTED_IDS
 from invenio.refextract_api import extract_references_from_record_xml, \
                                    FullTextNotAvailable
 from invenio.refextract_config import CFG_REFEXTRACT_VERSION
+from invenio.search_engine import perform_request_search
 
 AFS_HOME = '/afs/cern.ch/user/a/adeiana'
-AFS_HOME = '/Users/osso'
-DEST_DIR = 'refextract-results/%s' % CFG_REFEXTRACT_VERSION[36:]
+DEST_DIR = 'refextract-results/%s' % CFG_REFEXTRACT_VERSION[40:]
 
 
-def main(recids):
+def main(force=False):
+    recids = perform_request_search(p='', cc='HEP')
+
     # Create dest dir
     try:
         os.mkdir(os.path.join(AFS_HOME, DEST_DIR))
@@ -23,20 +25,25 @@ def main(recids):
         if done % 50 == 0:
             print 'done %s of %s' % (done + 1, len(recids))
 
+        filename = '%s.xml' % recid
+        dest_path = os.path.join(AFS_HOME, DEST_DIR, filename)
+        if not force and os.path.isfile(dest_path):
+            continue
+
         try:
             xml = extract_references_from_record_xml(recid)
             print 'processed', recid
         except FullTextNotAvailable:
             continue
 
-        filename = '%s.xml' % recid
-        out = open(os.path.join(AFS_HOME, DEST_DIR, filename), 'w')
-        out.write(xml.encode('utf-8'))
+        out = open(dest_path, 'w')
+        out.write(xml)
 
     print 'done'
 
 if __name__ == '__main__':
     try:
-        main(SELECTED_IDS)
+        force = '-f' in sys.argv
+        main(force=force)
     except KeyboardInterrupt:
         print 'Exiting'
