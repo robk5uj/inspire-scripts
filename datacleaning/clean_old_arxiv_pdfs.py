d6b007f219993f0244e259f4830cb91abded093d
Alessio Deiana <alessio.deiana@cern.ch>
2013-10-21 12:28:55 +0200

    Temporary changes to clean_old_arxiv.py

--- datacleaning/clean_old_arxiv_pdfs.py
+++ datacleaning/clean_old_arxiv_pdfs.py
@@ -8,17 +8,21 @@ def list_pdfs(recid):
     docs = rec_info.list_bibdocs()
 
     for doc in docs:
-        for ext in ('pdf', 'pdfa', 'PDF'):
-            try:
-                yield doc, doc.get_file(ext)
-            except InvenioBibDocFileError:
-                pass
+        for d in doc.list_all_files():
+            if d.get_format() in ('.pdf', '.pdfa', '.PDF'):
+                try:
+                    yield doc, d
+                except InvenioBibDocFileError:
+                    pass
 
 
 def main():
     now = datetime.now()
     datecut = now - timedelta(days=365)
-    for recid in perform_request_search(p="980__a:arxiv"):
+    # perform_request_search(p="980__a:arxiv")
+    for done, recid in enumerate([29556]):
+        if done % 500 == 0:
+            print done
         pdfs = list(list_pdfs(recid))
 
         max_version = 0
@@ -26,7 +30,8 @@ def main():
             max_version = max(max_version, pdf.get_version())
 
         for doc, pdf in pdfs:
-            if pdf.get_type() == 'arXiv' and pdf.get_version() < max_version and pdf.md < datecut:
+            if pdf.get_type() == 'arXiv' and pdf.get_version() < max_version and pdf.cd < datecut:
+                print 'deleting', pdf.get_format(), pdf.get_version(), 'from', recid
                 # We can clean this
                 doc.delete_file(pdf.get_format(), pdf.get_version())
 
