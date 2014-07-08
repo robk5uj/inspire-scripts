8ac3ae8ef6558f24b14b6cb7b7cfcd07f2fefcd0
Alessio Deiana <alessio.deiana@cern.ch>
2014-07-08 17:16:41 +0200

    Updates revert records script

--- datacleaning/revert_records.py
+++ datacleaning/revert_records.py
@@ -1,36 +1,28 @@
-from tempfile import mkstemp
-import os
-
 from invenio.bibedit_utils import get_record_revision_ids
 from invenio.bibeditcli import get_info_of_revision_id, \
                                get_marcxml_of_revision_id, \
                                save_xml_record
-from invenio.config import CFG_TMPSHAREDDIR
-from invenio.bibtask import task_low_level_submission
 
 
 def load_ids():
-    f = open('ids2.txt')
+    f = open('ids.txt')
     for line in f:
         if line.strip():
             yield int(line)
 
 
 def rollback_record(recid):
-        print 'id', recid
-        revisions = get_record_revision_ids(recid)
-        rev_count = 0
-        rev_info = get_info_of_revision_id(revisions[rev_count])
-        while 'arxiv-refs-fix' in rev_info:
-            rev_count += 1
-            rev_info = get_info_of_revision_id(revisions[rev_count])
-
-        if rev_count == 0:
-            print "%s not matching %s" % (recid, rev_info)
-        else:
-            print 'reverting to ', get_info_of_revision_id(revisions[rev_count])
-            xml_record = get_marcxml_of_revision_id(revisions[rev_count])
-            save_xml_record(recid, 0, xml_record)
+    print 'id', recid
+    revisions = get_record_revision_ids(recid)
+    rev_count = 0
+    rev_info = get_info_of_revision_id(revisions[rev_count])
+    if '/afs/cern.ch/user/s/sul/public/slacr.marcxml' not in rev_info:
+        print 'Conflicting Revisions'
+    else:
+        rev_count += 1
+        print 'reverting to ', get_info_of_revision_id(revisions[rev_count])
+        xml_record = get_marcxml_of_revision_id(revisions[rev_count])
+        save_xml_record(recid, 0, xml_record)
 
 
 def main():
